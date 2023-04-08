e53df4140df4822550e6f8c08fcda086a2c1538a
PC <PC@SCCTTO71361935>
2023-04-08 11:43:57 -0500

    comienzo de proyecto

---
+++ src/main/java/com/web/onpe/WebOnpeStsApplication.java
@@ -0,0 +1,13 @@
+package com.web.onpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebOnpeStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebOnpeStsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/web/onpe/controller/OnpeController.java
@@ -0,0 +1,5 @@
+package com.web.onpe.controller;
+
+public class OnpeController {
+
+}
