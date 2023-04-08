456bd71095b851ba636d91a623eed720ea8847ee
PC <PC@SCCTTO71361935>
2023-04-08 12:34:32 -0500

    controlador

--- src/main/java/com/web/onpe/controller/OnpeController.java
+++ src/main/java/com/web/onpe/controller/OnpeController.java
@@ -1,5 +1,19 @@
 package com.web.onpe.controller;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Configuration
+@CrossOrigin
+@Controller
+
 public class OnpeController {
+	
+    @GetMapping("/index")
+    public String Onpe() {
+	return "index";
+     }
 
 }
